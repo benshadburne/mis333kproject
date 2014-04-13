a39fd8e33886e34790401238afcde3d94d95aad5
dphelan92 <dphelan@utexas.edu>
2014-04-12 19:42:02 -0500

    Edited Cust_AddNewCustomer

--- Cust_AddNewCustomer.aspx.vb
+++ Cust_AddNewCustomer.aspx.vb
@@ -1,6 +1,14 @@
-﻿
+﻿'Author: Dennis Phelan
+'Purpose: Make sure Save and Clear buttons know what to do to Add a Customer
+'Date Created: April 11, 2014
+'Date Last Modified: April 11, 2014
+
 Partial Class Cust_AddNewCustomer
     Inherits System.Web.UI.Page
+
+    'Declare instances of classes
+    Dim CustDB As New DBCustomersClone
+    Dim valid As New ClassValidate
 
     Protected Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
 
