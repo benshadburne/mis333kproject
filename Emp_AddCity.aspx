59c1689cf6636e1586eaf5f9f70ebab4ebcd9fe5
aaryaman92 <aaryaman92@hotmail.com>
2014-04-12 20:49:17 -0500

    Worked on Add City page
    If you have any ideas for how to go to the next record in a table after
    a new page load without reloading the dataset, please talk to me.

--- Emp_AddCity.aspx
+++ Emp_AddCity.aspx
@@ -7,6 +7,7 @@
     <div id="header">
        Add Airport
    </div>
+    <asp:Panel runat="server" ID="pnlAddAirport"> 
    <div class ="label-default" id="middle">
 
         <asp:Label ID="Label1" runat="server" Text="City name:"></asp:Label>
@@ -36,6 +37,42 @@
         <asp:Button ID="btnAdd" runat="server" Text="Add" />
         <br /> 
         </div>
+    </asp:Panel>
+
+    <asp:Panel runat="server" ID="pnlAddInfo" Visible ="false"> 
+   <div class ="label-default" id="Div1">
+       <br />
+
+        <asp:Label ID="Label3" runat="server" Text="Mileage"></asp:Label>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Flight Time:"></asp:Label>
+        <br />
+
+<br />
+        <br />
+
+        <asp:Label ID="Label5" runat="server" Text="Add the mileage and flight time to " ForeColor="Red"></asp:Label>
+        <br />
+
+
+        </div>
+
+    <div class ="input-group-lg"id="Div2">
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="The city name is a required field." Text="*" ControlToValidate="txtCity"></asp:RequiredFieldValidator>
+        <br /> 
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="The airport code must be 3 letters." Text="*" ControlToValidate="txtAirport"></asp:RequiredFieldValidator>
+        <br /> 
+        <br /> 
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Add" />
+        <br /> 
+        </div>
+    </asp:Panel>
+
 
 
 
--- Emp_AddCity.aspx.vb
+++ Emp_AddCity.aspx.vb
@@ -1,8 +1,48 @@
 ﻿
 Partial Class Emp_AddCity
     Inherits System.Web.UI.Page
+    Dim DBAirport As New DBairportclone
 
     Protected Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'define a counter variable
+        Dim i As Integer
+        'define a variable to check if the airport code is unique
+
+        'get all airports with a stored procedure
+        DBAirport.GetALLairportcloneUsingSP()
+
+        i = DBAirport.MyView.Count - 1
+
+        'SHOULD THIS LOOP BE IN A CLASS? A DB CLASS??
+        'loop through airports to make sure the airport we are using is unique. 
+        'loop through i record
+        For j = 0 To i
+            'if the airport code for this table record is the same as the one entered...
+            If DBAirport.MyDataSet.Tables("tblAirportClone").Rows(j).Item("AirportCode") = txtAirport.Text Then
+                'show message and exit sub
+                lblMessage.Text = "That airport code is already in use. Please use another three letter combination."
+                Exit Sub
+            End If
+            'the record didn't match the one in that row of the database, go to next record
+        Next
+
+        'add record
+        DBAirport.AddAirport("usp_Airport_Add_New", txtAirport.Text, txtCity.Text)
+
+        'change which panel is visible
+        pnlAddAirport.Visible = False
+        pnlAddInfo.Visible = True
+
+        'use a loop to add session for each current airport to the page.
+        For j = 0 To i
+            Session.Add("Airport" + j.ToString, DBAirport.MyDataSet.Tables("tblAirportClone").Rows(j).Item("AirportCode"))
+        Next
+
+        'FIND OUT IF THERE IS A WAY TO FORCE THE USER TO GO THROUGH ALL OF THE AIRPORTS WITHOUT CLOSING THE PAGE OR GOING SOMEWHERE ELSE
+        'allow the user to add the info for the first airport. 
+        'remove the session variable for the first airport. 
+        'add flight information 
+        'reload the page and go to the next session variable
 
     End Sub
 End Class
