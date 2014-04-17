8312efdef3f8600121935ac43a658b11794f35cc
bhshad <bshadburne@fstichem.com>
2014-04-17 16:29:51 -0500

    Created Checkin Login Class
    this will be super helpful and necessary for all cust/employee pages

---
+++ App_Code/ClassCheckLogin.vb
@@ -0,0 +1,25 @@
+﻿Option Strict On
+Imports Microsoft.VisualBasic
+
+
+
+Public Class ClassCheckLogin
+
+    'PURPOSE OF THIS CLASS IS TO MAKING CHECKING EMPLOYEE/CUSTOMER LOGINS EASIER
+
+
+    Public Function CheckEmployee(ByVal strID As String) As Boolean
+
+
+        Return False
+
+    End Function
+
+    Public Function CheckCustomer(ByVal strID As String) As Boolean
+
+
+
+        Return False
+    End Function
+
+End Class
