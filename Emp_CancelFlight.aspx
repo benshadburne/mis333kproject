ca0f7d50dd6f5a4d88aeec7bd2bd7e3c77569ebb
mavsman22 <jace.barton@gmail.com>
2014-04-28 16:20:33 -0500

    Starting Cancel Flight

---
+++ Emp_CancelFlight.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/MasterPage.master" AutoEventWireup="false" CodeFile="Emp_CancelFlight.aspx.vb" Inherits="Emp_CancelFlight" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+        <div id ="left">
+        <div class ="label-primary">
+            
+            <asp:ValidationSummary ID="ValidationSummary1" runat="server" /><br /><br />
+            <asp:Label ID="lblMessage" runat="server" Text="Welcome to the Cancel Flight Page! Please select a flight to cancel."></asp:Label>
+
+        </div>
+        <div class ="btn">
+            <br /><br />
+            <br /><br />
+        <asp:Button ID="btnCancel" runat="server" Text="Cancel" /><br />
+            <asp:Button ID="btnAbort" runat="server" Text="Abort" Visible="False" /><br />
+            <asp:Button ID="btnConfirm" runat="server" Text="Confirm" Visible="False" />
+            </div>
+
+    </div>
+
+    <div id ="middle">
+
+<asp:Label ID="Label2" runat="server" Text="Flight Number"></asp:Label><br />
+            <asp:Label ID="Label3" runat="server" Text="Departure City"></asp:Label><br />
+            <asp:Label ID="Label4" runat="server" Text="Arrival City"></asp:Label><br />
+            <asp:Label ID="Label5" runat="server" Text="Departure Time"></asp:Label><br />
+            <asp:Label ID="Label6" runat="server" Text="Arrival Time"></asp:Label><br />
+            <asp:Label ID="Label7" runat="server" Text="Base Fare"></asp:Label><br />
+        <asp:Label ID="Label9" runat="server" Text="Days Flown"></asp:Label><br />
+
+        
+        </div>
+        <div id ="right">
+            <div class ="input-group-lg">
+                <asp:DropDownList ID="ddlFlights" runat="server" AutoPostBack="True"></asp:DropDownList>
+                <br />
+                <asp:TextBox ID="txtDepartureCity" runat="server" ReadOnly="True"></asp:TextBox><br />
+                <asp:Textbox ID="txtArrivalCity" runat="server" ReadOnly="True"></asp:Textbox><br />
+                <asp:DropDownList ID="ddlDepartureTimeHour" runat="server" AutoPostBack="True" Enabled="False">
+                    <asp:ListItem>00</asp:ListItem>
+                    <asp:ListItem>1</asp:ListItem>
+                    <asp:ListItem>2</asp:ListItem>
+                    <asp:ListItem>3</asp:ListItem>
+                    <asp:ListItem>4</asp:ListItem>
+                    <asp:ListItem>5</asp:ListItem>
+                    <asp:ListItem>6</asp:ListItem>
+                    <asp:ListItem>7</asp:ListItem>
+                    <asp:ListItem>8</asp:ListItem>
+                    <asp:ListItem>9</asp:ListItem>
+                    <asp:ListItem>10</asp:ListItem>
+                    <asp:ListItem>11</asp:ListItem>
+                    <asp:ListItem>12</asp:ListItem>
+                    <asp:ListItem>13</asp:ListItem>
+                    <asp:ListItem>14</asp:ListItem>
+                    <asp:ListItem>15</asp:ListItem>
+                    <asp:ListItem>16</asp:ListItem>
+                    <asp:ListItem>17</asp:ListItem>
+                    <asp:ListItem>18</asp:ListItem>
+                    <asp:ListItem>19</asp:ListItem>
+                    <asp:ListItem>20</asp:ListItem>
+                    <asp:ListItem>21</asp:ListItem>
+                    <asp:ListItem>22</asp:ListItem>
+                    <asp:ListItem>23</asp:ListItem>
+                </asp:DropDownList>
+                <asp:Label ID="Label1" runat="server" Text=":"></asp:Label>
+                <asp:DropDownList ID="ddlDepartureTimeMinutes" runat="server" AutoPostBack="True" Enabled="False">
+                    <asp:ListItem>00</asp:ListItem>
+                    <asp:ListItem>15</asp:ListItem>
+                    <asp:ListItem>30</asp:ListItem>
+                    <asp:ListItem>45</asp:ListItem>
+                </asp:DropDownList><br />
+                <asp:Label ID="lblArrivalTime" runat="server" Text="Arrival Time"></asp:Label><br />
+                <asp:TextBox ID="txtBaseFare" runat="server" Enabled="False"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvBaseFare" runat="server" ErrorMessage="Please enter a base fare!" ControlToValidate="txtBaseFare" Text="*"></asp:RequiredFieldValidator><br />
+                <asp:CheckBoxList ID="cblDaysToFly" runat="server" Enabled="False">
+                    <asp:ListItem>Monday</asp:ListItem>
+                    <asp:ListItem>Tuesday</asp:ListItem>
+                    <asp:ListItem>Wednesday</asp:ListItem>
+                    <asp:ListItem>Thursday</asp:ListItem>
+                    <asp:ListItem>Friday</asp:ListItem>
+                    <asp:ListItem>Saturday</asp:ListItem>
+                    <asp:ListItem>Sunday</asp:ListItem>
+                </asp:CheckBoxList>
+        </div>
+            </div>
+</asp:Content>
+
---
+++ Emp_CancelFlight.aspx.vb
@@ -0,0 +1,151 @@
+﻿Option Strict On
+Partial Class Emp_CancelFlight
+    Inherits System.Web.UI.Page
+
+    'create instance of flight database class
+    Dim FObject As New DBFlightsClone
+
+    'create instance of mileage database class
+    Dim MObject As New DBMileage
+
+    'create instance of calculation database class
+    Dim CObject As New ClassCalculate
+
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'if first time loading page, set datasource of ddlFlights
+        If IsPostBack = False Then
+            'get all flights into database
+            FObject.GetALLFlightsCloneUsingSP()
+            ddlFlights.DataSource = FObject.MyDataSet.Tables("tblFlightClone")
+            ddlFlights.DataTextField = "FlightNumber"
+            ddlFlights.DataBind()
+            ddlFlights.SelectedIndex = 0
+            LoadInformationFromFlight(ddlFlights.SelectedIndex)
+        End If
+
+    End Sub
+
+    'sub will load all needed information into rest of fields once user enters flight number
+    Public Sub LoadInformationFromFlight(intIndex As Integer)
+        'set variables for hours and minutes 
+        Dim strDepartureTimeRaw As String
+        Dim strDepartureTimeHour As String
+        Dim strDepartureTimeMinute As String
+        Dim intDepartureTime As Integer
+        Dim strArrivalTime As String
+
+        FObject.GetALLFlightsCloneUsingSP()
+        'load departure city
+        txtDepartureCity.Text = FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("DepartureCity").ToString
+        'load arrival city
+        txtArrivalCity.Text = FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("EndCity").ToString
+        'load departure time
+        strDepartureTimeRaw = FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("DepartureTime").ToString
+        If strDepartureTimeRaw.Length = 3 Then
+            strDepartureTimeHour = strDepartureTimeRaw.Substring(0, 1)
+            strDepartureTimeMinute = strDepartureTimeRaw.Substring(1, 2)
+        ElseIf strDepartureTimeRaw.Length = 4 Then
+            strDepartureTimeHour = strDepartureTimeRaw.Substring(0, 2)
+            strDepartureTimeMinute = strDepartureTimeRaw.Substring(2, 2)
+        Else
+            lblMessage.Text = "Something has gone horribly wrong."
+            Exit Sub
+        End If
+
+        ddlDepartureTimeHour.SelectedValue = strDepartureTimeHour
+        ddlDepartureTimeMinutes.SelectedValue = strDepartureTimeMinute
+        'load arrival time
+        intDepartureTime = CInt(FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("DepartureTime"))
+        'create variable for duration of flight
+        Dim intDuration As Integer
+
+        'find duration
+        Dim aryParamNamesMileage As New ArrayList
+        Dim aryParamValuesMileage As New ArrayList
+
+        'add param names to array list
+        aryParamNamesMileage.Add("@StartAirport")
+        aryParamNamesMileage.Add("@EndAirport")
+
+        'add param values to array list
+        aryParamValuesMileage.Add(txtDepartureCity.Text)
+        aryParamValuesMileage.Add(txtArrivalCity.Text)
+
+        'find duration of flight
+        MObject.FindDuration(aryParamNamesMileage, aryParamValuesMileage)
+        intDuration = CInt(MObject.MyDataSet.Tables("tblMileageClone").Rows(0).Item(0))
+
+        'calculate the arrival time that needs to appear
+        strArrivalTime = CObject.CalculateArrivalTime(intDepartureTime, intDuration)
+        'create session variable for arrival time
+        Session("ArrivalTime") = strArrivalTime
+
+        'put it in label
+        lblArrivalTime.Text = strArrivalTime
+
+        'load base fare
+        txtBaseFare.Text = FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("BaseFare").ToString
+        'load days flown
+        'load Monday
+        If FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("Monday").ToString = "Y" Then
+            cblDaysToFly.Items(0).Selected = True
+        End If
+
+        'load Tuesday
+        If FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("Tuesday").ToString = "Y" Then
+            cblDaysToFly.Items(1).Selected = True
+        End If
+
+        'load Wednesday
+        If FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("Wednesday").ToString = "Y" Then
+            cblDaysToFly.Items(2).Selected = True
+        End If
+
+        'load Thursday
+        If FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("Thursday").ToString = "Y" Then
+            cblDaysToFly.Items(3).Selected = True
+        End If
+
+        'load Friday
+        If FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("Friday").ToString = "Y" Then
+            cblDaysToFly.Items(4).Selected = True
+        End If
+
+        'load Saturday
+        If FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("Saturday").ToString = "Y" Then
+            cblDaysToFly.Items(5).Selected = True
+        End If
+
+        'load Sunday 
+        If FObject.MyDataSet.Tables("tblFlightClone").Rows(intIndex).Item("Sunday").ToString = "Y" Then
+            cblDaysToFly.Items(6).Selected = True
+        End If
+
+    End Sub
+
+    Protected Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+
+    End Sub
+
+    Protected Sub btnAbort_Click(sender As Object, e As EventArgs) Handles btnAbort.Click
+
+    End Sub
+
+    Protected Sub btnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
+
+    End Sub
+
+    Public Sub EnterConfirmAbortMode()
+        btnCancel.Visible = False
+        btnAbort.Visible = True
+        btnConfirm.Visible = True
+
+    End Sub
+
+    Public Sub ProtectedMode()
+        btnCancel.Visible = True
+        btnAbort.Visible = False
+        btnConfirm.Visible = False
+    End Sub
+End Class
