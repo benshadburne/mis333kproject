cca05dabc147c5530e4f198b66c88e5edf774322
bhshad <bshadburne@fstichem.com>
2014-05-01 01:59:15 -0500

    minor changes

--- HomePage.aspx.vb
+++ HomePage.aspx.vb
@@ -2,7 +2,7 @@
 Partial Class _Default
     Inherits System.Web.UI.Page
 
-    Protected Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+    Protected Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click, btn2.Click, btn3.Click, btn4.Click
         Dim txtUsername As TextBox = CType(Master.FindControl("txtUsername"), TextBox)
 
         txtUsername.Text = ""
